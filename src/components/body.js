d1d71885b8bc283bcb7fb16c184e380595b9ff8b
NassCode <63391304+NassCode@users.noreply.github.com>
2023-06-14 13:35:22 +0400

    pick date via select tags

--- src/components/body.js
+++ src/components/body.js
@@ -1,14 +1,59 @@
 //import DatePicker from "./datePicker"
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 import { useState } from "react";
+import Selector from "./selector";
 
 const dayjs = require("dayjs");
 
 function Body() {
   const currentDate = dayjs(new Date());
   const [age, setAge] = useState("");
+  const [newAge, setNewAge] = useState('')
+  const [datesFromSelector, setDatesFromSelector] = useState([]);
   let selectedDate;
   let diffInDaysMonthYear = 0;
+  let diffInDay = 0;
+
+  function getDatesFromSelector(dates) {
+    console.log(dates);
+    let inputToDate = dayjs(`${dates[0]}/${dates[1]}/${dates[2]}`)
+    console.log(inputToDate.$d)
+    diffInDay = currentDate.diff(inputToDate.$d, 'days')
+    console.log(diffInDay)
+
+    function getAgeInMonthsAndDays(days) {
+      const years = Math.floor(days / 365.25);
+      const months = Math.floor((days % 365.25) / 30.44);
+      const remainingDays = Math.floor((days % 365.25) % 30.44);
+
+      if (years === 0 && months === 0) {
+        return `${remainingDays} ${remainingDays === 1 ? "day" : "days"}`;
+      } else if (years === 0 && remainingDays === 0) {
+        return `${months} ${months === 1 ? "month" : "months"}`;
+      } else if (months === 0 && remainingDays === 0) {
+        return `${years} ${years === 1 ? "year" : "years"}`;
+      } else if (years === 0) {
+        return `${months} ${
+          months === 1 ? "month" : "months"
+        } and ${remainingDays} ${remainingDays === 1 ? "day" : "days"}`;
+      } else if (months === 0) {
+        return `${years} ${
+          years === 1 ? "year" : "years"
+        } and ${remainingDays} ${remainingDays === 1 ? "day" : "days"}`;
+      } else if (remainingDays === 0) {
+        return `${years} ${years === 1 ? "year" : "years"} and ${months} ${
+          months === 1 ? "month" : "months"
+        }`;
+      } else {
+        return `${years} ${years === 1 ? "year" : "years"}, ${months} ${
+          months === 1 ? "month" : "months"
+        } and ${remainingDays} ${remainingDays === 1 ? "day" : "days"}`;
+      }
+    }
+
+    setNewAge(getAgeInMonthsAndDays(diffInDay))
+    
+  }
 
   function calculateAge(e) {
     selectedDate = dayjs(e);
@@ -47,15 +92,20 @@ function Body() {
     setAge(getAgeInMonthsAndDays(diffInDaysMonthYear));
   }
 
+
+
   return (
     <div
       style={{ display: "flex", flexDirection: "column", alignItems: "center" }}
     >
-      <div>
+      {/* <div>
         <DatePicker
           maxDate={dayjs(new Date()).subtract(1, "day")}
           onAccept={(e) => calculateAge(e.$d)}
         />
+      </div> */}
+      <div>
+        <Selector getDates={getDatesFromSelector}/>
       </div>
 
       <div
@@ -67,6 +117,7 @@ function Body() {
         }}
       >
         {age && <span>Age: {age}</span>}
+        {newAge && <span>Age: {newAge}</span>}
       </div>
     </div>
   );
---
+++ src/components/selector.js
@@ -0,0 +1,117 @@
+import { useState } from "react";
+import * as React from "react";
+import Box from "@mui/material/Box";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import Button from '@mui/material/Button';
+
+
+
+function Selector({ getDates }) {
+  const [day, setDay] = useState("");
+  const [month, setMonth] = useState("");
+  const [year, setYear] = useState("");
+
+  const handleDayChange = (event) => {
+    setDay(event.target.value);
+  };
+
+  const handleMonthChange = (event) => {
+    setMonth(event.target.value);
+  };
+
+  const handleYearChange = (event) => {
+    setYear(event.target.value);
+  };
+
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  // array of leap years between 1920 and 2023
+  const leapYears = [
+    1920, 1924, 1928, 1932, 1936, 1940, 1944, 1948, 1952, 1956, 1960, 1964,
+    1968, 1972, 1976, 1980, 1984, 1988, 1992, 1996, 2000, 2004, 2008, 2012,
+    2016, 2020, 2024,
+  ];
+
+  return (
+    <div style={{display: 'flex', flexDirection: 'column'}}>
+      <div style={{ display: "flex", flexDirection: "row", gap: '20px' }}>
+        <Box sx={{ minWidth: 80 }}>
+          <FormControl fullWidth>
+            <InputLabel id="demo-simple-select-label">Day</InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              variant="standard"
+              value={day}
+              label="Day"
+              onChange={handleDayChange}
+            >
+              {Array.from(Array(31).keys()).map((i) => {
+                return <MenuItem key={i} value={i + 1}>{i + 1}</MenuItem>;
+              })}
+            </Select>
+          </FormControl>
+        </Box>
+        <Box sx={{ minWidth: 80 }}>
+          <FormControl fullWidth>
+            <InputLabel id="demo-simple-select-label">Month</InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              variant="standard"
+              value={month}
+              label="Month"
+              onChange={handleMonthChange}
+            >
+              {months.map((m, i) => {
+                return (
+                  <MenuItem key={i + 1} value={m}>
+                    {i + 1} - {m}
+                  </MenuItem>
+                );
+              })}
+            </Select>
+          </FormControl>
+        </Box>
+        <Box sx={{ minWidth: 80 }}>
+          <FormControl fullWidth>
+            <InputLabel id="demo-simple-select-label">Year</InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              variant="standard"
+              value={year}
+              label="Year"
+              onChange={handleYearChange}
+            >
+              {Array.from(Array(104).keys()).map((i) => {
+                return <MenuItem key={i} value={2023 - i}>{2023 - i}</MenuItem>;
+              })}
+            </Select>
+          </FormControl>
+        </Box>
+      </div>
+      <div style={{display: 'flex', justifyContent: 'center', marginTop: '20px'}}>
+        <Button variant="text" onClick={() => getDates([day, month, year])}>Calculate</Button>
+        </div>
+    </div>
+  );
+}
+
+export default Selector;
